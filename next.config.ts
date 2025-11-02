460a8f9780793994c1c951bb18acd154f38961da
M. Sertaç Toröz <msertactoroz@gmail.com>
2025-11-02 20:59:20 +0300

    chore(config): disable Next.js dev tools indicator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+   devIndicators: false, // Hide  bottom-Left Next.js Icon 
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
